77ae3c28e84273e271f2305152e204ff61c8c7ca
Hlare <57828255+EuroNerv@users.noreply.github.com>
2019-11-19 21:12:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+import datetime
 import requests
+
+mode = {
+    "day" : False,
+}
 
 token = "igrgeRUTnr6PSNkLmMfY2RIV0D3BDcdVibYqkx/UDwakdUcCUWpt8Es7r8poEyPi+Rp7L1Xh1dunbzd2ofMPPaB1oBJvCsNCs36j/PMEzEsiWhdeqjaa/5nwt3iEvAaaWrLzEEchP/BKY4lOWE/9xQdB04t89/1O/w1cDnyilFU="
 
@@ -9,4 +14,25 @@ def LineNotify(token,message):
    headers = {'Authorization': 'Bearer ' + line_notify_token} 
    requests.post(line_notify_api, data=payload, headers=headers)
 
-LineNotify(token,"これはテスト送信です")
+def center():
+    time = datetime.datetime.now()
+    if time.hour == 0:
+       if mode["day"] != True:
+          mode["day"] = True
+          yea = time.year
+          mont = time.month
+          today = time.day
+          dtA = datetime.datetime(yea,mont,today)
+          dtB = datetime.datetime(2020,1,18)
+          theday = dtB - dtA
+          num = theday.days
+          message = "\n日付が変わりました\n今日は" + str(mont) + "月" + str(today) + "日です\nセンター試験まで残り" + str(num) + "日です。合格目指して頑張りましょう"
+          LineNotify(token,message)
+    if time.hour == 8:
+       if mode["day"] == True:
+          mode["day"] = False
+    if time.hour == 10:
+       if mode["day"] == True:
+          mode["day"] = False
+while True:
+ center()
